ff9c3c8ffc9e9324fdd5c76ab82dd076aac5aa5a
Shridhar <shridhar.p@zerodha.com>
2021-11-29 23:03:39 +0530

    fix: created doctype to tag users

---
+++ robinhood/robinhood/doctype/tag_users/tag_users.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, zerodha and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Tag Users', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ robinhood/robinhood/doctype/tag_users/test_tag_users.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, zerodha and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestTagUsers(unittest.TestCase):
+	pass
