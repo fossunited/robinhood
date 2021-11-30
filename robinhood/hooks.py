fc9aa5cb21c324586dd32c91d4e8c9603e008f44
Shridhar <shridhar.p@zerodha.com>
2021-11-30 19:25:13 +0530

    fix: export web page/template as fixtures

--- robinhood/hooks.py
+++ robinhood/hooks.py
@@ -182,3 +182,30 @@ app_license = "MIT"
 # 	"robinhood.auth.validate"
 # ]
 
+fixtures = [
+    {
+        "dt": "Web Page",
+        "filters": [
+            [
+                "module",
+                "in",
+                ("Robinhood")
+            ]
+        ]
+    },
+    {
+        "dt": "Web Template",
+        "filters": [
+            [
+                "module",
+                "in",
+                (
+                    "Robinhood"
+                )
+            ]
+        ]
+    }
+    # "Property Setter",
+    # "Custom Script",
+    # "Role"
+]
