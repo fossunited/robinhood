ba261446b3643bc753442269adb1f7c924d19aa8
nikhilponnuru <nikhil.kp@zerodha.com>
2021-12-01 18:03:23 +0530

    feat: add chapter, subchapter, city doctypes

---
+++ robinhood/robinhood/doctype/robinhood_chapter/robinhood_chapter.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, zerodha and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Robinhood Chapter', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ robinhood/robinhood/doctype/robinhood_sub_chapter/robinhood_sub_chapter.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, zerodha and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Robinhood Sub Chapter', {
+	// refresh: function(frm) {
+
+	// }
+});
