1ebee068945088a96468c22a69eac0eee22edfe7
Shridhar Patil <shridharpatil2792@gmail.com>
2022-01-11 20:00:22 +0530

    Revert "feat: add centurion and ninja certificate designs and updates to cert… (#4)" (#5)
    This reverts commit 17dacc1f5f976524f98aecb33fb1ed38ea74b46c.

--- robinhood/robinhood/doctype/checkin/checkin.py
+++ robinhood/robinhood/doctype/checkin/checkin.py
@@ -2,37 +2,23 @@
 # For license information, please see license.txt
 
 
-import time
-
 import frappe
 from frappe.model.document import Document
-from frappe.utils import get_url
 from frappe.utils.background_jobs import enqueue
-from pdf_text_overlay import pdf_from_template
+# from pdf_text_overlay import pdf_from_template
 
 
 class Checkin(Document):
-    def generate_certificate(self, checkin_count):
+    def generate_certificate(self):
         """
-        Generate a certificate after every 10 and 100 checkins to be sent to the respective robin.
+        Generate a certificate after every N checkins to be sent to the respective robin.
         """
         jinja_data = {
             "robin_name": frappe.db.get_value(
-                "User", {"email": self.owner}, ["first_name"]
+                "User", {"email": self.user}, ["first_name"]
             ),
-            "base_url": get_url(),
-            # "base_url": "http://0.0.0.0:8010",
-            "robin_location": frappe.db.get_value(
-                "Robin Chapter Mapping", {"user": self.owner}, ["chapter"]
-            ),
-            "certificate_date": time.strftime("%d %B %Y"),  # 12 December 2022
+            "checkins_milestone": 50,
         }
-
-        certificate_filename = None
-        if checkin_count == 10:
-            certificate_filename = "ninja.html"
-        elif checkin_count == 100:
-            certificate_filename = "centurion.html"
 
         with open(
             frappe.get_app_path(
@@ -41,7 +27,7 @@ class Checkin(Document):
                 "doctype",
                 "checkin",
                 "certificate",
-                certificate_filename,
+                "certificate.html",
             )
         ) as htmlfile:
             html_str = htmlfile.read()
@@ -54,28 +40,32 @@ class Checkin(Document):
                 "fname": filename,
                 "fcontent": filecontent,
             }
-
             frappe.sendmail(
-                recipients=[self.owner],
-                subject="Congratulations! You won a certificate in recognition to your work",
-                message="Congratulations! You won a certificate in recognition to your work",
+                recipients=self.user,
+                subject="Congratulations! You won a certificate in recognition to your \
+                    work",
+                message="Enjoy",
                 attachments=[certificate_pdf],
                 delayed=False,
             )
+
+    def before_insert(self):
+        self.user = frappe.session.user
 
     def after_insert(self):
         res = frappe.db.sql(
             """
         SELECT COUNT(*) AS count
         FROM `tabCheckin`
-        WHERE owner=%s
-        GROUP BY owner
+        WHERE user=%s
+        GROUP BY user
+        HAVING COUNT(*)>%s
         """,
-            [self.owner],
+            [self.user, 20],
             as_dict=True,
         )
-        if res and res[0]["count"] in [10, 100]:
-            enqueue(self.generate_certificate, checkin_count=res[0]["count"])
+        if res and res[0]["count"]:
+            enqueue(self.generate_certificate)
 
 
 @frappe.whitelist()
@@ -116,12 +106,9 @@ def checkins(city):
 @frappe.whitelist(allow_guest=True)
 def top_robins(city):
     """Top ronins."""
-    return frappe.db.sql(
-        """SELECT c.name,
+    return frappe.db.sql("""SELECT c.name,
         c.selfie, c.creation, c.location, c.meals_served
         from `tabCheckin` c
         WHERE c.creation >= now() - INTERVAL 60 DAY
             and c.city = %(city)s
-        order by date(c.creation)""",
-        {"city": city},
-    )
+        order by date(c.creation)""", {"city": city})
