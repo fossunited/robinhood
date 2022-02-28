85771431eb647be68c203e41ac43bad004b9308f
nikhilponnuru <nikhil.kp@zerodha.com>
2022-02-28 18:52:24 +0530

    fix: fix permission issue with certificate log and add option to send certificate from robin certificate  log itself

--- robinhood/robinhood/doctype/robin_certificate_log/robin_certificate_log.py
+++ robinhood/robinhood/doctype/robin_certificate_log/robin_certificate_log.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2022, zerodha and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from robinhood.robinhood.doctype.checkin.checkin import Checkin
+
 
 class RobinCertificateLog(Document):
-	pass
+    def before_insert(self):
+        c = frappe.new_doc("Checkin")
+        c.owner = self.robin
+        c.generate_certificate(int(self.type_of_certificate))
