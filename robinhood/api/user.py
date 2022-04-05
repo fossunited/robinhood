0d4296210ec0687430f09df8b4334059b8317927
nikhilponnuru <nikhil.kp@zerodha.com>
2022-04-05 12:12:58 +0530

    feat: add prefilling of user porfile page details, tiitle case words in certificate and top robins web template

--- robinhood/api/user.py
+++ robinhood/api/user.py
@@ -2,9 +2,29 @@ import frappe
 
 
 def update(doc, method=None):
-    if not frappe.db.sql("SELECT name from `tabRobin Chapter Mapping` where chapter = %(city)s", {"city": doc.city}):
-        frappe.get_doc({
-            "doctype": "Robin Chapter Mapping",
-            "user": frappe.session.user,
-            "chapter": doc.city
-        }).save(ignore_permissions=True)
+    if not frappe.db.sql(
+        "SELECT name from `tabRobin Chapter Mapping` where chapter = %(city)s",
+        {"city": doc.city},
+    ):
+        frappe.get_doc(
+            {
+                "doctype": "Robin Chapter Mapping",
+                "user": frappe.session.user,
+                "chapter": doc.city,
+            }
+        ).save(ignore_permissions=True)
+
+
+@frappe.whitelist()
+def fetch_profile_details():
+    res = frappe.db.sql(
+        """
+    SELECT first_name, last_name, mobile_no, city, profile_picture
+    FROM `tabUser profile update`
+    WHERE owner=%s
+    """,
+        [frappe.session.user],
+    )
+    if res:
+        return res
+    return []
