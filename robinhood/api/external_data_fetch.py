f78d9f4dcef5eaa23a63c1af3a7cfc61db2141f1
kundra28 <113905427+kundra28@users.noreply.github.com>
2023-03-04 14:13:02 +0530

    Update external_data_fetch.py
    adding a function for facebook share

--- robinhood/api/external_data_fetch.py
+++ robinhood/api/external_data_fetch.py
@@ -14,3 +14,8 @@ def fetch_weekly_food_stats():
     response = requests.get(url="https://robinhoodarmy.com/joinnowmail/getMealStats", headers=headers).text
     
     return response
+
+
+@frappe.whitelist(allow_guest=True)
+def facebook_share(link):
+    requests.get(url='https://www.facebook.com/dialog/feed?app_id=1077099302859935&display=popup&link='+ urllib.parse.quote(link) +'&redirect_uri=https://developers.facebook.com/tools/explorer')
